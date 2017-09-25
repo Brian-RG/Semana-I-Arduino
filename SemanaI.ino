25796cfbda40e51957d4e05fb4c1f8e67e775f74
Brian R <brianreyesg@hotmail.com>
2017-09-25 10:53:17 -0500

    Add files via upload

---
+++ SemanaI.ino
@@ -0,0 +1,11 @@
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+  Serial.println("Bienvenidos");
+  delay(300);
+  Serial.println("Semana i");
+  delay(300);
+
+}
---
+++ Semana_I_2.ino
@@ -0,0 +1,22 @@
+
+void setup() {
+  pinMode(3,INPUT_PULLUP);
+  pinMode(4,INPUT_PULLUP);
+  Serial.begin(9600);
+}
+
+void loop() {
+    if(digitalRead(3)==LOW and digitalRead(4)==LOW){
+      Serial.println("Estás presionando ambos botones");
+      delay(1000);
+    }
+  else if(digitalRead(3)==LOW){
+      Serial.println("Estás presionando el botón A");
+      delay(1000);
+    }
+  else if(digitalRead(4)==LOW){
+      Serial.println("Estás presionando el botón B");
+      delay(1000);
+    }
+
+}
