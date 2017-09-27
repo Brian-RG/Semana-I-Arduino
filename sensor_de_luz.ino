be4a91df5b1642408d05c423c033734fded61634
Brian R <brianreyesg@hotmail.com>
2017-09-27 08:38:48 -0500

    Add files via upload

---
+++ sensor_de_luz.ino
@@ -0,0 +1,17 @@
+int Sensor=2;
+int lectura;
+int luz;
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(Sensor,OUTPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  lectura=analogRead(Sensor);
+  luz = map (lectura,450 ,910 ,0 ,100);
+  Serial.println(luz);
+  delay(100);
+}
+
